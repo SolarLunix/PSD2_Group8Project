8e006c41dcedfb307cdd88f75febd5fa979553c5
Melissa <solarlunix@gmail.com>
2020-11-25 18:44:15 +0000

    Added a test package and created two sample test programs

---
+++ AirportCheckIn/src/TicketPackage/Test/TestPlane.java
@@ -0,0 +1,15 @@
+package TicketPackage.Test;
+
+/*******
+ *   AirportCheckIn:TicketPackage.Test
+ *   File: TestPlane
+ *   Created by: Melissa Melaugh
+ *   Created on: 25/11/2020
+ *   Updated on: 25/11/2020
+ *   Project Description: //TODO
+ *******/
+public class TestPlane {
+    public static void main(String[] args) {
+        
+    }
+}
